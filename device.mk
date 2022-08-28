5e638b6d18075e8bb27e96623539fe5758c3d34d
Akash Srivastava <akashniki@gmail.com>
2022-08-28 14:23:03 +0530

    ginkgo : Switch to QTI perf component
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -271,13 +271,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
-# Perf
+# perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
-    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Platform
 TARGET_BOARD_PLATFORM := trinket
@@ -299,7 +295,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     vibrator \
     wfd \
     telephony \
-    wlan
+    wlan \
+    perf
 
 # Ramdisk
 PRODUCT_PACKAGES += \
