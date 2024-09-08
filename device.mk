bb76c828f4dcdbe6c662a3f581ce741f59a4b9be
Wrdn28 <ifloraxd@gmail.com>
2024-09-08 20:45:33 +0000

    Revert "ginkgo: Checkout perf config from clo"
    This reverts commit 253590634266880fa67f2c1861214affa6f4ae1b.

--- device.mk
+++ device.mk
@@ -303,12 +303,6 @@ PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 TARGET_BOARD_PLATFORM := trinket
 TRINKET := trinket
 
-# Perf
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
-
 # QTI VNDK Framework Detect
 PRODUCT_ODM_PROPERTIES += \
     ro.vendor.qti.va_odm.support=1
