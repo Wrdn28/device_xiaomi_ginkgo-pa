ef2d251d2ceb6cd8a4d427c45f019f8e08a239b0
Wrdn28 <ifloraxd@gmail.com>
2024-09-21 08:27:55 +0000

    ginkgo: drop frameworks sensorservice 1.0
    *already define by source
    
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- device.mk
+++ device.mk
@@ -312,8 +312,6 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
-    android.frameworks.sensorservice@1.0 \
-    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
