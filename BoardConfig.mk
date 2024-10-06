bd8ceed059e8f394f607ef9d008a307ab8e0b6d7
Wrdn28 <ifloraxd@gmail.com>
2024-10-06 09:16:44 +0000

    Revert "ginkgo: drop compatibility_matrix"
    This reverts commit 3732cf42a8f1b68e3299c3b848c104d92e948c3c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,7 @@ BOARD_HAS_QCA_FM_SOC := "cherokee"
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_WILLOW_FILES := $(DEVICE_PATH)/configs/hidl/manifest_willow.xml
 ODM_MANIFEST_SKUS += willow
 
