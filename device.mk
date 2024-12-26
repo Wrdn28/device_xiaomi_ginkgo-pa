842749ffc0d996ff5beb252563be74e4b3784f71
Wrdn28 <ifloraxd@gmail.com>
2024-12-26 16:28:34 +0000

    Revert "Ginkgo: Enable some android go features, even useful for non go devices"
    This reverts commit d9ea648cf3a60ed29f0ea5dbd5946d2eff902416.

--- device.mk
+++ device.mk
@@ -183,13 +183,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
 
-# Go features
-
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
