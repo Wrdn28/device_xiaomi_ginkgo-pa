3e8fc008ef2151ee19b459ee5d69063c3cb4aa22
Wrdn28 <ifloraxd@gmail.com>
2025-01-02 13:19:55 +0000

    ginkgo: Xpe bringup
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/xperience_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    xperience_ginkgo-userdebug \
+    xperience_ginkgo-eng
---
+++ xperience_ginkgo.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/xperience/config/common.mk)
+
+# Inherit from lavender device
+$(call inherit-product, device/xiaomi/ginkgo/device.mk)
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+WITH_GMS := false
+
+PRODUCT_NAME := xperience_ginkgo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := ginkgo
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 8/8T
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
